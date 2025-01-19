26f7129b914cdf8750cd6a207cc8a65b2a5b0dab
Kai Hong Fung <kfung75@my.bcit.ca>
2025-01-19 00:26:17 -0800

    added geocoding functionality

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,5 @@
 let map;
+let geocoder;
 
 // fetch API key
 async function fetchApiKey() {
@@ -32,7 +33,7 @@ async function initMap() {
   // Request needed libraries
   const { Map } = await google.maps.importLibrary("maps");
   //const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
+  geocoder = new google.maps.Geocoder();
   map = new Map(document.getElementById("map"), {
     zoom: 10,
     center: position,
@@ -46,5 +47,21 @@ async function initMap() {
     title: "Uluru",
   });*/
 }
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
 
 fetchApiKey();
